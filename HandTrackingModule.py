cac17811bd3678d07d0edc1396b8e699a7591abc
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-10-03 22:12:40 -0700

    fixing some stuff for first test

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -4,11 +4,39 @@ import mediapipe as mp
 import random
 import time
 
+class handDetector():
+    def __init__(self, mode=False,maxHands = 2, detectionCon = 0.5, trackCon = 0.5):
+        # make object and each object will have its own variable
+        self.mode = mode
+        self.maxHands = maxHands
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+        self.mpHands = mp.solutions.hands 
+        self.hands = self.mpHands.Hands(self.mode,self.maxHands,self.detectionCon, self.trackCon)# defult parmeters
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def findHands(self,img,draw = True):
+
+        imgRgb = cv.cvtColor(flipped_img, cv.COLOR_BGR2RGB)
+        results = self.hands.process(imgRgb)
+
+        if results.multi_hand_landmarks:
+        #get info from each hand/ loops thru each hand
+            for handLms in results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(flipped_img, handLms, self.mpHands.HAND_CONNECTIONS)
+                # for id, lm in enumerate(handLms.landmark):
+                #     #print(id,lm)
+                #     h, w, c = img.shape
+                #     cx, cy = int(lm.x*w), int(lm.y*h)
+                #     cv.circle(flipped_img, (cx,cy), 5, (255,255,255), cv.FILLED)
+                # #print(id, cx, cy)
+                # #if id == 8:
+
 
 cap = cv.VideoCapture(0)
 success, img = cap.read()
-mpHands = mp.solutions.hands 
-hands = mpHands.Hands()# defult parmeters
+
 
 
    
@@ -16,32 +44,16 @@ hands = mpHands.Hands()# defult parmeters
     flipped_img = cv.flip(img, 1) # Flip horizontally
     window_h, window_w, c = flipped_img.shape
     
-    imgRgb = cv.cvtColor(flipped_img, cv.COLOR_BGR2RGB)
-    results = hands.process(imgRgb)
-    mpDraw = mp.solutions.drawing_utils
+    
+    
     
    
 
         
     #print(CoinRangeX,CoinRangeY)
 
-    if results.multi_hand_landmarks:
-        
-        #get info from each hand/ loops thru each hand
-        for handLms in results.multi_hand_landmarks:
-            #not drawing on rgb img cuz we are not displaying the img
-            # draw land marks of single hand
-            mpDraw.draw_landmarks(flipped_img, handLms, mpHands.HAND_CONNECTIONS)
-            for id, lm in enumerate(handLms.landmark):
-                #print(id,lm)
-                h, w, c = img.shape
-                cx, cy = int(lm.x*w), int(lm.y*h)
-                
-                
-                #print(id, cx, cy)
-                if id == 8:
-                     cv.circle(flipped_img, (cx,cy), 5, (255,255,255), cv.FILLED)
-          
+    
+                     
 
     cv.imshow(" Image", flipped_img)
     key = cv.waitKey(1) & 0xFF
