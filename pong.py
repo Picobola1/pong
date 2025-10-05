3d18e456b07fd5a18d207fede96c86821896f6fb
Ellie Bonilla <bonilla.ellie@gmail.com>
2025-10-04 18:40:22 -0700

    can detect wich side hand is on

--- pong.py
+++ pong.py
@@ -17,27 +17,41 @@ paddleSpawn = False
 tipIds = [4,8,12,16,20]
 
 while True:
+    
+
+    
+
     success, img = cap.read()
     img = cv.flip(img, 1)
     img = detector.findHands(img)
+
+    h, w, c = img.shape
+    middle_x = w // 2
+
+    cv.line(img, (middle_x, 0), (middle_x, h), (0, 255, 0), 2)
+
     if detector.results.multi_hand_landmarks:
         for i in range(len(detector.results.multi_hand_landmarks)):
             lmList = detector.findPosition(img, handNum=i, Draw=False)
         #if hand is there
             if len(lmList) != 0:
-                
+                HandSide = detector.results.multi_handedness[i].classification[0].label
                 for id in range(0,5):
                     if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
                         paddleSpawn = False
                     else:
                         for id, lm in enumerate(lmList):
                             h, w, c = img.shape
-                            cx, cy = lmList[12][1], lmList[12][2]
+                            cx, cy = lmList[14][1], lmList[14][2]
                             
-                            if id == 12:
+                            if id == 6:
                                 paddleSpawn = True
-                                if paddleSpawn == True:
+                                if paddleSpawn == True and HandSide == "Left":
                                     img[cy:cy+114, cx:cx+33] = paddle1
+                                    
+                                if paddleSpawn == True and HandSide == "Right":
+                                    img[cy:cy+118, cx:cx+33] = paddle2
+                                    
 
                         
            
